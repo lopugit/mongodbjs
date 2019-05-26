db7746f274db67c700445e580821815313c9ec91
lopugit <ireply@myleisure.com.au>
2019-05-26 15:17:34 +1000

    did I do that?

--- tests/MongoClient.connect.js
+++ tests/MongoClient.connect.js
@@ -4,7 +4,7 @@ let Uri = require('sentience')['uri.v2.js']
 (async () => {
 	let MongoClient = require('mongodb').MongoClient
 	
-	let uri = `mongodb://test1:test1@127.0.0.1:27017/admin?authSource=admin`
+	let uri = `mongodb://test1:test1@localhost:27017/admin?authSource=admin`
 	console.log(uri)
 	// MongoClient.connect(uri, {useNewUrlParser: true}, function(err, res){
 	// 	console.log(err)
@@ -14,5 +14,5 @@ let Uri = require('sentience')['uri.v2.js']
 	console.log(connection && connection.length !== 0)
 })()
 
-// 'mongodb://test1:test1@127.0.0.1:27017/admin?authMechanism=DEFAULT&authSource=admin'
-// 'mongodb://test1:test1@127.0.0.1:27017/admin?authMechanism=DEFAULT&authSource=admin'
+// 'mongodb://test1:test1@localhost:27017/admin?authMechanism=DEFAULT&authSource=admin'
+// 'mongodb://test1:test1@localhost:27017/admin?authMechanism=DEFAULT&authSource=admin'
